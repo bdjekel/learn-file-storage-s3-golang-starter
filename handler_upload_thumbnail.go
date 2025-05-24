fc16cc781e23f95c622e0fc08db2f35993076909
bdjekel <bdjekel@gmail.com>
2025-05-23 23:11:05 -0500

    adding "upload thumbnail" functionality. Currently getting an Error dialog box in the browser ==> Error: Unexpected non-whitespace character after JSON at position 232 (line 1 column 233)

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -31,7 +32,50 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20 // 10 megabytes
+	if err := r.ParseMultipartForm(maxMemory); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse multipart form", err)
+		return
+	}
+
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse file", err)
+		return
+	}
+
+	mediaType := header.Header.Get("Content-Type")
+	imageData, err := io.ReadAll(r.Body)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse image data.", err)
+	}
+	videoMetadata, err := cfg.db.GetVideo(userID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "User not authorized to retreive this video.", err)
+	}
+
+	newTN := thumbnail{
+		data: imageData,
+		mediaType: mediaType,
+	}
+
+	videoThumbnails[videoMetadata.ID] = newTN
+
+	tnURL := fmt.Sprintf("http://localhost:<port>/api/thumbnails/%v", videoMetadata.ID)
+	videoMetadata.ThumbnailURL = &tnURL
+
+	if err := cfg.db.UpdateVideo(videoMetadata); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video metadata.", err)
+	}
+	
+	updatedVideo, err := cfg.db.GetVideo(userID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "User not authorized to retreive this video.", err)
+	}
+
+	respondWithJSON(w, http.StatusOK, updatedVideo)
+	
+	defer file.Close()
 
 	respondWithJSON(w, http.StatusOK, struct{}{})
 }
